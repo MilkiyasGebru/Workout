164f9c4d22049322961db667347315fb883def3a
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-02 19:39:24 +0300

    Added the server

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const express = require('express');
+const database = require('./database');
+const workoutRoutes = require('./routes/workouts');
+
+
+// Initialize Express App
+const app = express();
+
+
+app.use(express.json());
+
+app.use((req,res,next) => {
+    console.log(req.method);
+    console.log(req.body)
+    next();
+})
+
+
+app.use('/api/workouts',workoutRoutes);
+
+
+// Connect to database
+database()
+    .then(()=>{
+        //Listen for requests
+        app.listen(process.env.PORT,()=>{
+            console.log("Server is listening on port 4000!!")
+        })
+    })
+    .catch((err)=>{console.log(err)})
+
+
+
+
+
