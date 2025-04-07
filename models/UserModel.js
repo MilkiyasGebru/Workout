48b2837f58a71d0bdf93d3a5835a770434cd13c5
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-07 20:30:44 +0300

    Added a static method to the user schema for the signup

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +14,21 @@ const userSchema = new Schema({
         required: true,
     }
 })
+
+// Static Method
+userSchema.statics.signup = async (email, password) => {
+    const exists = await this.findOne({email: email});
+
+    if (exists) {
+        throw new Error(`${email} already exists`);
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashed_password = await bcrypt.hash(password, salt)
+
+    const registered_user = await this.create({email:email,password:hashed_password})
+
+    return registered_user
+}
 
 module.exports = mongoose.model('User', userSchema);
