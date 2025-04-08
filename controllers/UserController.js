685a6473bb20c89ece7eb53b110faa5023367920
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-08 21:11:06 +0300

    Updated the user controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,24 @@
+const User = require('../models/UserModel');
 
 // Login User
 const loginUser = async (req, res) => {}
 
 
 // Signup User
-const signupUser = async (req, res) => {}
+const signupUser = async (req, res) => {
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.signup(email, password);
+        res.status(201).json(user);
+    }
+    catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
+
+
+module.exports = {
+    loginUser,
+    signupUser
+}
